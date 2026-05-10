08a6352ecdd20f757b280e33e86a2260d107ae85
Betül Zeybek <153202604+betulzeyybek@users.noreply.github.com>
2026-05-10 17:47:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file is not None:
     img = Image.open(uploaded_file).convert("RGB")
-st.image(img, caption="Yüklenen X-Ray Görüntüsü", use_column_width=True)
+    st.image(img, caption="Yüklenen X-Ray Görüntüsü", use_column_width=True)
     img_resized = img.resize((224, 224))
     img_array = np.array(img_resized, dtype=np.float32) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
